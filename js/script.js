5baf3c25e08a0463d1a3b8cd2b4c68235b686393
Anatolii pr <toljani496149@gmail.com>
2021-07-07 12:26:48 +0300

    first test commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use sctrict";
+
+let number = 4.3;
+console.log(-4/0);
+console.log('string' * 3); 
+
+const persone = "Alex";
+
+const check = true;
+const check2 = false;
+ 
+// console.log(something);
+
+let coco;
+console.log(coco);
+
+const obj = {
+     name: "John",
+     age: 25,
+     isMarried: false
+};
+// console.log(obj.age);
+console.log(obj["name"]);
+
+let arr = ['null.jpg', 'total.png', 6];
+console.log(arr[0]);
---
+++ js/script2.js
@@ -0,0 +1,33 @@
+"use strict";
+// const test = confirm("Are you here?");
+// console.log(test);
+
+// const ansver = +prompt("Do you gave 18?", "1111");
+// console.log(typeof(ansver));
+
+// const answers = [];
+// answers [0] = prompt("Your name");
+// answers [1] = prompt("Your second");
+// answers [2] = prompt("Your surrname");
+
+// document.write(answers);
+
+// const test1 = 'Toys';
+// console.log(`https://sourct.com/${test1}/2/q4twrtg`);
+
+// const user = 'Ivan';
+// alert(`Hello, ${user}`);
+
+// let incr = 10,
+//     decr = 10;
+// ++incr;
+// --decr;
+// console.log(incr);
+// console.log(decr);
+
+const forOne = true,
+    forTwice = false;
+
+console.log(forOne && !forTwice);
+
+    
